7fadd3f3068f98711873ebefab42cdc139c5f060
Any Gyozalyan <102807142+AnyIIGy@users.noreply.github.com>
2022-07-21 23:14:45 +0400

    CallBack
    CallBack

---
+++ index11.js
@@ -0,0 +1,32 @@
+/*JS - ում ֆունկցիաները object-ներ են և կարող են որպես արգումենտ ստանալ կամ return անել այլ ֆունկցիա։
+Այն ֆունկցիաները,որոնք փոխանցվում են կոչվում են` CallBack ֆունկցիաներ (իհարկե,կարող ենք տալ ցանկացած անուն)*/
+
+function doTask(subject, callback) {
+
+    console.log(`Starting my ${subject} task`);
+
+    callback(); // => function callback () {console.log('Finished my task')}
+    /* Կանչում ենք callBack-ը որ երևա իրան ինչ արժեք ենք փոխանցել,
+    որպես արժեք իրան տվել ենք,որ դառնա Ֆունկցիա callback անունով,
+    որնել տպումա ՝ finished my task ' */
+}
+
+doTask('CallBack', function () {
+    console.log('Finished my task');
+});
+
+//#2
+function doTask2(subject, callback) {
+
+    console.log(`Starting my ${subject} task-2`);
+
+    callback();
+    /* Callback -ին որպես արժեք փոխանցել (վերագրել) ենք callBackFinished ֆունկցիան,
+    որն էլ կանչում ենք doTask2 -ի մեջ */
+}
+
+function CallBackFinished() {
+    console.log('Finished my task-2');
+}
+
+doTask2('CallBack', CallBackFinished);
